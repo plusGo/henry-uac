0a2784fd25671ab7c0c94bcf0f4ea0bd66863781
henry <mhlplus@outlook.com>
2022-02-26 20:19:56 +0800

    feat:nacos配置中心

--- src/main/java/com/henry/uac/UACServerApplication.java
+++ src/main/java/com/henry/uac/UACServerApplication.java
@@ -1,11 +1,13 @@
 package com.henry.uac;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.henry.uac.mapper")
+@NacosPropertySource(dataId = "henry-uac", autoRefreshed = true, groupId = "henry")
 public class UACServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(UACServerApplication.class, args);
